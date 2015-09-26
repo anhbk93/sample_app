458a95afc755bbf55e9575f7bfb19a91b8689122
anhnvbk93 <anh.nguyen.it.dut@gmail.com>
2015-09-27 00:02:18 +0700

    [Add feature] Finish log in / log out feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find(params[:id])
-    # debugger
-  end 
-
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -19,8 +20,9 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
 
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
